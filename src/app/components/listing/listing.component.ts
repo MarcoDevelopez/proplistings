10ef29359da9cdbd6a865ccdb5c1371730ee21f7
Marco López <mlopezdev@gmail.com>
2017-04-03 16:23:41 -0500

    Angular 2 Project with Firebase

---
+++ src/app/components/listing/listing.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '../../services/firebase.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import * as firebase from 'firebase';
+
+@Component({
+  selector: 'app-listing',
+  templateUrl: './listing.component.html',
+  styleUrls: ['./listing.component.css']
+})
+export class ListingComponent implements OnInit {
+
+  id:any;
+  listing:any;
+  imageUrl:any;
+
+  constructor(
+    private fireBaseService:FirebaseService,
+    private router:Router,
+    private route:ActivatedRoute) { }
+
+  ngOnInit() {
+    // Get ID
+    this.id = this.route.snapshot.params['id'];
+    this.fireBaseService.getListingDetails(this.id).subscribe(listing => {
+      this.listing = listing;
+      console.log(this.listing.path);
+      let storageRef = firebase.storage().ref();
+      let spaceRef = storageRef.child(this.listing.path);
+      storageRef.child(this.listing.path).getDownloadURL().then((url) => {
+        // Set image url
+        this.imageUrl = url;
+      }).catch((error) => {
+        console.log(error);
+      })
+    });
+  }
+
+  onDeleteClick() {
+    this.fireBaseService.deleteListing(this.id);
+    this.router.navigate(['/listings']);
+  }
+
+}
